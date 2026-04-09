8d4214c6d080ac3fb33e2b811f6872dfb048bf50
saraung <saraungbabu@gmail.com>
2026-04-09 20:23:38 +0530

    index up

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
-// app/index.tsx
+
 import { Redirect } from "expo-router";
 import { useAuth } from "@/hooks/useAuth";
 
 export default function Index() {
   const { isAuthenticated, isLoading } = useAuth();
 
-  // Wait until token restore finishes
+
   if (isLoading) {
-    return null; // splash screen can be shown here later
+    return null;
   }
 
   if (!isAuthenticated) {
